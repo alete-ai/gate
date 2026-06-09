b609caffea3490bfe53a02ec8ef31baa6b60dd50
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2026-06-09 19:18:59 -0400

    Set library type to static to avoid Catalyst dynamic linking issues

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     products: [
         .library(
             name: "AleteGateKit",
-            type: .dynamic,
+            type: .static,
             targets: ["AleteGateKit"]),
     ],
     dependencies: [
